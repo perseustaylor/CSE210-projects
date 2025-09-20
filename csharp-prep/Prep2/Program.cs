8f66a54dddb45c60005eeac8b7349ec822d86df4
Perseus Taylor <perseustaylor16@gmail.com>
2025-09-20 16:39:51 -0600

    very similar to python

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What was your grade percentage? ");
+        string userNumberString = Console.ReadLine();
+        int userNumberValue = int.Parse(userNumberString);
+
+        if (userNumberValue >= 90)
+        {
+            Console.WriteLine("Your grade is A");
+        }
+        else if (userNumberValue >= 80)
+        {
+            Console.WriteLine("Your grade is B");
+        }
+        else if (userNumberValue >= 70)
+        {
+            Console.WriteLine("Your grade is C");
+        }
+        else if (userNumberValue >= 60)
+        {
+            Console.WriteLine("Your grade is D");
+        }
+        else
+        {
+            Console.WriteLine("Your grade is F");
+        }
     }
 }
