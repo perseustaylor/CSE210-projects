8b357e518f160863211bc94748d18b8bf5fcccd1
Perseus Taylor <perseustaylor16@gmail.com>
2025-09-20 16:17:49 -0600

    chanedToCamelCase

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,9 +5,9 @@ class Program
     static void Main(string[] args)
     {
         Console.Write("What is your first name? ");
-        string firstname = Console.ReadLine();
+        string firstName = Console.ReadLine();
         Console.Write("What is your last name? ");
-        string lastname = Console.ReadLine();
-        Console.WriteLine($"Your name is, {lastname} {firstname} {lastname}!");
+        string lastName = Console.ReadLine();
+        Console.WriteLine($"Your name is, {lastName} {firstName} {lastName}!");
     }
 }
