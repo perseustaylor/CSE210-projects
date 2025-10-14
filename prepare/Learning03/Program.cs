f99555e70dbd1a47e0c36b1a8abf847f1b5f62f6
Perseus Taylor <perseustaylor16@gmail.com>
2025-10-14 17:00:53 -0600

    encapsulation practice

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction fraction = new Fraction();
+        Fraction fraction1 = new Fraction(7);
+        Fraction fraction2 = new Fraction(2, 2);
+
+        Console.WriteLine(fraction.GetFractionAsString());
+        Console.WriteLine(fraction.GetNumberValue());
+
+        Console.WriteLine(fraction1.GetFractionAsString());
+        Console.WriteLine(fraction1.GetNumberValue());
+
+        Console.WriteLine(fraction2.GetFractionAsString());
+        Console.WriteLine(fraction2.GetNumberValue());
+
+
     }
 }
