dd0f0952f81559b33d3e200dbfb96dbb7f05868c
Perseus Taylor <perseustaylor16@gmail.com>
2025-11-11 19:56:02 -0700

    shapes

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Shape circle = new Circle("Red", 5.0);
+        shapes.Add(circle);
+
+        Shape square = new Square("Blue", 4.0);
+        shapes.Add(square);
+
+        Shape rectangle = new Rectangle("Green", 3.0, 6.0);
+        shapes.Add(rectangle);
+
+        // Console.WriteLine($"A circle of color {circle.GetColor()}, has an area of{circle.GetArea()}");
+        // Console.WriteLine($"A Square of color {square.GetColor()}, has an area of {square.GetArea()}");
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"A shape of color {shape.GetColor()}, has an area of {shape.GetArea()}");
+        }
     }
 }
